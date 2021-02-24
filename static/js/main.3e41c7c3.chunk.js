86e742a3984f00e6d98c85eb1e7b664eb8c4c9be
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2021-02-24 12:39:59 +0900

    Updates

---
+++ static/js/main.3e41c7c3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptemplate=this.webpackJsonptemplate||[]).push([[0],{10:function(t,e,n){},16:function(t,e,n){"use strict";n.r(e);var c=n(0),r=n.n(c),o=n(4),i=n.n(o),s=(n(9),n(10),n(1));var a=function(){return Object(s.jsx)("h1",{children:" Hello "})};n(12);i.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},9:function(t,e,n){}},[[16,1,2]]]);
+//# sourceMappingURL=main.3e41c7c3.chunk.js.map
